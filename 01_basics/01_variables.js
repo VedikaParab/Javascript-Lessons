a5a5c0b0c180a2c73943fc5742a2a23317e9df04
Vedika Parab <vedikaparab204@gmail.com>
2025-09-03 21:34:52 +0530

    basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345;
+let accountName = "Vedika";
+var accountEmail = "vedikaparab204@gmail.com";
+accountState = "Mumbai"; //this method is not preferred
+
+// const is used to declare permanent values
+// prefer to use let instead of var
+// because of issue of block scope and function scope
+
+console.table([accountId, accountName, accountEmail, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //to treat all JS Code as new version
+
+// alert(3+3)  // we are using nodejs, not browser
+
+console.log(3+3) 
+
+// number
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+// object 
+
+// undefined is of type undefined but null is of type object
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
